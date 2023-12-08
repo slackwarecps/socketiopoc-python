fb4c40dd562d60beeb479d7b603a86884fb9a279
fabiopereirazup <fabio.pereira@zup.com.br>
2023-12-08 19:26:23 -0300

    ok funcionando na porto 8081

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ socketio pocket exemplo
 ````
 $ rodar
 
-``
+``````
+<script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-from flask import make_response
 
 app = Flask(__name__)
-
-
+#socketio = SocketIO(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -15,16 +12,10 @@ values = {
     'slider2': 0,
 }
 
-#rota html
 @app.route('/')
 def index():
-    # Enable Access-Control-Allow-Origin
-    response = make_response(render_template('index.html',**values))
+    return render_template('index.html',**values)
 
-    response.headers.set("Batata", "frita")
-    return response
-
-#rota de eventos
 @socketio.on('connect')
 def test_connect():
     emit('after connect',  {'data':'Lets dance'})
@@ -33,8 +24,7 @@ def test_connect():
 def value_changed(message):
     values[message['who']] = message['data']
     emit('update value', message, broadcast=True)
+    print(values)
 
-# Notice how socketio.run takes care of app instantiation as well.
 if __name__ == '__main__':
-    #192.168.1.103
-    socketio.run(app,port=5001,host="0.0.0.0",debug=True)
+    socketio.run(app,port=8081, host='0.0.0.0')
